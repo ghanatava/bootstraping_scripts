9ce50d84068674441e21d94dc91f90929170a354
ghanatava <ghanatva@gmail.com>
2023-12-16 13:58:47 +0530

    added bootstrap script for jenkins server

---
+++ jenkins-server.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+sudo apt update -y
+
+sudo apt install openjdk-17-jre -y
+sudo apt install openjdk-17-jdk -y
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update -y
+sudo apt install jenkins -y
+sudo systemctl enable jenkins
+
+#installing docker 
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo systemctl enable docker
+
+#docker post installation steps
+sudo groupadd docker || true
+sudo usermod -aG docker ubuntu         #replace ubuntu with your username or use $USER
+sudo usermod -aG docker jenkins
+
+#reboot the instance to make sure that changes apply
+sudo reboot
